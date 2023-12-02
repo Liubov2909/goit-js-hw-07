d836aa09270ce9dcde3f82e319128030949e1b0e
Liubov2909 <ostapcuk2909@gmail.com>
2023-12-02 18:03:32 +0200

    dzcorect

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,8 @@
-const categoriesList = document.getElementById('categories');
-const categoriesItems = categoriesList.querySelectorAll('li.item');
-
-console.log(`Total categories: ${categoriesItems.length}`);
-
-categoriesItems.forEach(category => {
-  const categoryName = category.querySelector('h2').textContent;
-  const categoryItemsCount = category.querySelectorAll('li').length;
-
-  console.log(`Category: ${categoryName}`);
-  console.log(`Number of items in the category: ${categoryItemsCount}`);
+const categories = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categories.length}`);
+const info = categories.forEach(category => {
+    const title = category.firstElementChild.textContent;
+    console.log(`Category: ${title}`);
+    const elements = category.lastElementChild.childElementCount;
+    console.log(`Elements: ${elements}`);
 });
